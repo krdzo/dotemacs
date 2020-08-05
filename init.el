8c47b5397d2ef94caf7ddbc21e017bfde9be9ab3
krdzo <krdzodev@gmail.com>
2020-08-05 15:24:41 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,38 @@
+;; Change default for faster startup 
+(setq gc-cons-percentage 0.6)
+(setq gc-cons-threshold (* 100 1024 1024)) ;; 100MB
+
+;; Mesuring startup
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Init package and use-package
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq package-archives '(("org"       . "https://orgmode.org/elpa/")
+			 ("gnu"       . "https://elpa.gnu.org/packages/")
+			 ("melpa"     . "https://melpa.org/packages/")
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")))
+(require 'package)
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(setq-default use-package-always-ensure t)
+
+(use-package org
+  :ensure t)
+
+
+
+(org-babel-load-file (expand-file-name "pravila.org" user-emacs-directory))
+
