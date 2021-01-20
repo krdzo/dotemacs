d3461c0e21986e98c99bd46eb8f9d31eb160f465
krdzo <krdzodev@gmail.com>
2021-01-20 01:59:03 +0100

    Some little init.el changes

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 ;; Change default for faster startup 
 (setq gc-cons-percentage 0.6)
-(setq gc-cons-threshold (* 256 1024 1024)) ;; 100MB
+(setq gc-cons-threshold (* 64 1024 1024)) ;; 100MB
 
 ;; Mesuring startup
 (add-hook 'emacs-startup-hook
@@ -20,6 +20,7 @@
 			 ("melpa-stable" . "https://stable.melpa.org/packages/")))
 (require 'package)
 
+;; to see what use-package does you can use pp-macroexpand-last-sexp
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
@@ -28,9 +29,10 @@
 (setq-default use-package-always-ensure t)
 ;; (setq use-package-compute-statistics t)
 
-;; (define-key global-map (kbd "C-c x") 'use-package-report)
+(define-key global-map (kbd "C-c x") 'use-package-report)
 
-(require 'org)
-(add-to-list 'org-modules 'org-tempo t)
-(org-babel-load-file (expand-file-name "pravila.org" user-emacs-directory))
+(use-package org
+  :config
+  (add-to-list 'org-modules 'org-tempo t)
+  (org-babel-load-file (expand-file-name "pravila.org" user-emacs-directory)))
 
