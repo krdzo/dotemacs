1cda06dede7eca51a2ec018a40a2b427a59cd2a6
krdzo <krdzodev@gmail.com>
2021-02-15 22:15:07 +0100

    A little bit of everything

--- early-init.el
+++ early-init.el
@@ -2,3 +2,7 @@
 ;; Doesn't change load times, it's just more visualy appealing like this
 (setq frame-inhibit-implied-resize t)
 
+;; ovo je za brze loadovanje emacs-a
+;; ali cu ga tek ukljuciti kad budem imao vremena da se
+;; zazam sa ovim sad sam ga samo postavio radi podsecanja
+;; (setq generated-autoload-file t)
