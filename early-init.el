ac716cbad2739301930be7e70584d3a632dea2b5
Krdzo <krdzodev@gmail.com>
2021-02-23 21:16:29 +0100

    Some formating changes

--- early-init.el
+++ early-init.el
@@ -1,6 +1,13 @@
 ;; Do not resize the frame at this early stage.
 ;; Doesn't change load times, it's just more visualy appealing like this
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(setq inhibit-startup-screen t)
 (setq frame-inhibit-implied-resize t)
+(setq inhibit-startup-buffer-menu t)
+(setq inhibit-startup-echo-area-message "ivan")
+
+
 
 ;; ovo je za brze loadovanje emacs-a
 ;; ali cu ga tek ukljuciti kad budem imao vremena da se
