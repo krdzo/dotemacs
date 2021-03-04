829fd98ce33b33e745489f1e5b720be168b84896
Krdzo <krdzodev@gmail.com>
2021-03-04 16:01:07 +0100

    Malo Org, malo Dired, i malo menjanje gde pisu keybindings.

--- init.el
+++ init.el
@@ -3,13 +3,6 @@
 (setq gc-cons-threshold (* 64 1024 1024)) ;; 100MB
 (setq read-process-output-max (* 1024 1024))
 
-
-;; trying to build pravila.el every time pravila.org file is saved
-;; insted of every time emacs is started
-(defvar kr-org-conf-file (expand-file-name "pravila.org" user-emacs-directory))
-(defvar kr-el-conf-file (expand-file-name "pravila.el" user-emacs-directory))
-
-
 ;; Mesuring startup
 (add-hook 'emacs-startup-hook
           (lambda ()
@@ -33,13 +26,19 @@
 (eval-when-compile
   (require 'use-package))
 (setq-default use-package-always-ensure t)
-(setq use-package-verbose t)
 
+(setq use-package-verbose t)
 (setq use-package-compute-statistics t)
 (define-key global-map (kbd "C-c x") 'use-package-report)
 
 
+(defvar kr-org-conf-file (expand-file-name "pravila.org" user-emacs-directory)
+  "Path to org file of my config")
+(defvar kr-el-conf-file (expand-file-name "pravila.el" user-emacs-directory)
+  "Path to tangled org file of my config")
+
 (if (file-exists-p kr-el-conf-file)
     (load-file kr-el-conf-file)
     (org-babel-load-file kr-org-conf-file))
+
 
