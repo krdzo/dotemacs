af17359a8d816a74f9121e740d42f30af4d7c060
krdzo <krdzodev@gmail.com>
2021-07-16 19:12:49 +0200

    Emacs 28 conf

--- init.el
+++ init.el
@@ -1,3 +1,6 @@
+(when (version<  "28" emacs-version)
+    (setq straight-base-dir "~/.emacsen/"))
+
 (defvar kr/config-org "init-kr.org"
   "Name of my config file")
 (org-babel-load-file (expand-file-name kr/config-org user-emacs-directory))
