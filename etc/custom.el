ae4366639bf1b14e398847a9352b1e4f0a9d2488
krdzo <krdzodev@gmail.com>
2022-04-29 15:56:37 +0200

    Enable auto-insert mode

--- etc/custom.el
+++ etc/custom.el
@@ -9,5 +9,9 @@
    '("1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "f0eb51d80f73b247eb03ab216f94e9f86177863fb7e48b44aacaddbfe3357cf1" "281904c0e4ef7e2e36d0433c2d28b9b46bcc173f00b563a163256ae3e543c33c" default))
  '(safe-local-variable-values
    '((auto-insert-alist
-      (lisp-mode "Skeleton for this direcotry." ";(ql:quickload :rutils)" n ";(named-readtables:in-readtable  rtl:rutils-readtable)")))))
+      (lisp-mode "Skeleton for this direcotry." ";(ql:quickload :rutils)" n ";(named-readtables:in-readtable rtl:rutils-readtable)")))))
 (custom-set-faces)
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
