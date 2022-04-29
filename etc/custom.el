0153759cd58289e9f157f4862b99ff35c7d58e03
krdzo <krdzodev@gmail.com>
2022-04-29 15:14:46 +0200

    Add ./etc/ folder to git

---
+++ etc/custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-safe-themes
+   '("1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "f0eb51d80f73b247eb03ab216f94e9f86177863fb7e48b44aacaddbfe3357cf1" "281904c0e4ef7e2e36d0433c2d28b9b46bcc173f00b563a163256ae3e543c33c" default))
+ '(safe-local-variable-values
+   '((auto-insert-alist
+      (lisp-mode "Skeleton for this direcotry." ";(ql:quickload :rutils)" n ";(named-readtables:in-readtable  rtl:rutils-readtable)")))))
+(custom-set-faces)
